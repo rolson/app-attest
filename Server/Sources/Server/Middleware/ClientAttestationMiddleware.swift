8667594a0d1f919547619cce43b4c286197b0749
Ryan Olson <rolson@esri.com>
2026-06-04 16:25:37 -0600

    challenge + replay protection

--- Server/Sources/Server/Middleware/ClientAttestationMiddleware.swift
+++ Server/Sources/Server/Middleware/ClientAttestationMiddleware.swift
@@ -1,5 +1,6 @@
 import AttestationDecoding
 import AttestationValidation
+import Fluent
 import Vapor
 
 /// A middleware that enforces requests must provide an Attestation Object from Apple's DeviceCheck framework.
@@ -21,11 +22,28 @@ actor ClientAttestationMiddleware: AsyncMiddleware {
               let assertionToken = request.headers.bearerAuthorization?.token,
               let assertion = Data(base64Encoded: assertionToken),
               !assertion.isEmpty
-            // TODO: validate the assertion using a stored challenge and public key.
-            // validator.validate(assertion: assertion, challenge: challenge, keyID: keyID)
         else {
             throw Abort(.unauthorized)
         }
+
+        // Require a single, fresh challenge per key and consume it to prevent replay.
+        let challenges = try await request.db.query(IssuedChallenge.self)
+            .filter(\.$keyID, .equal, keyID)
+            .all()
+
+        try await challenges.delete(force: true, on: request.db)
+
+        guard challenges.count == 1,
+              let challenge = challenges.first,
+              let twoMinutesAgo = Calendar.current.date(byAdding: .minute, value: -2, to: .now),
+              let creationDate = challenge.createdAt,
+              creationDate > twoMinutesAgo
+        else {
+            throw Abort(.unauthorized)
+        }
+
+        // TODO: validate the assertion using the consumed challenge and stored key material.
+        // try await validator.validate(assertion: assertion, challenge: challenge.challenge, keyID: keyID)
 
         return try await next.respond(to: request)
     }
